3735be15d6caf7fb06521b172611ab4c874b7bb6
3888 <krrg@mail.ru>
2020-12-28 19:36:10 +0200

    added Either

--- src/main/kotlin/material/exceptions/Either.kt
+++ src/main/kotlin/material/exceptions/Either.kt
@@ -1,14 +1,67 @@
 package material.exceptions
 
 import arrow.core.Either
+import kotlin.system.measureNanoTime
 
 fun main() {
+//    either()
+    eitherVsTryCatch()
+
 
 }
 
-private fun parse(s: String): Either<NumberFormatException, Int> =
-    if (s.matches(Regex("-?[0-9]+"))) {
-        Either.Right(s.toInt())
-    } else {
-        Either.Left(NumberFormatException("$s is not a valid integer."))
+private fun either() {
+    val x = parse("123")
+    val y = parse("123dfgh")
+    println(x)
+    println(y)
+
+    val valueX = when (x) {
+        is Either.Left -> when (x.a) {
+            is NumberFormatException -> "Not a number!"
+            else -> "Unknown error"
+        }
+        is Either.Right -> "Number that was parsed: ${x.b}"
     }
+
+    println(valueX)
+
+    val valueY = when (y) {
+        is Either.Left -> when (y.a) {
+            is NumberFormatException -> "Not a number!"
+            else -> "Unknown error"
+        }
+        is Either.Right -> "Number that was parsed: ${y.b}"
+    }
+
+    println(valueY)
+}
+
+private fun parse(value: String): Either<NumberFormatException, Int> =
+    if (value.matches(Regex("-?[0-9]+"))) {
+        Either.Right(value.toInt())
+    } else {
+        Either.Left(NumberFormatException("$value is not a valid integer."))
+    }
+
+private fun eitherVsTryCatch() {
+    val value = "123ss"
+
+    val sequenceInNanosEither = measureNanoTime {
+        println(parse(value))
+    }
+    println("sequenceInNanosEither обработана за $sequenceInNanosEither наносекунд")
+
+    val sequenceInNanosTryCatch = measureNanoTime {
+        try {
+            if (value.matches(Regex("-?[0-9]+"))) {
+                println(value.toInt())
+            } else {
+                println(value.toInt())
+            }
+        } catch (e: Exception) {
+            println(e)
+        }
+    }
+    println("sequenceInNanosTryCatch обработана за $sequenceInNanosTryCatch наносекунд")
+}
