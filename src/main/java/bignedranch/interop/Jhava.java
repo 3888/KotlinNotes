b0a76995fda9cef05f95780d6e6489474c426eb1
3888 <krrg@mail.ru>
2021-01-04 10:57:15 +0200

    refactored Java files

--- src/main/java/bignedranch/interop/Jhava.java
+++ src/main/java/bignedranch/interop/Jhava.java
@@ -1,19 +1,48 @@
 package bignedranch.interop;
 
+import bignerdranch.interop.Hero;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class Jhava {
 
-    public int hitPoints = 52489112;
+    private int hitPoints = 52489112;
+    private String greeting = "BLARGH";
 
     @NotNull
     public String utterGreeting() {
-        return "BLARGH";
+        return greeting;
     }
 
     @Nullable
     public String determineFriendshipLevel() {
         return null;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+
+    public void offerFood() {
+        Hero.handOverFood("pizza");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Hero.makeProclamation()); // is OK in case @file:JvmName("Hero") before package
+//        System.out.println(HeroKt.makeProclamation()); //is OK in case no @file:JvmName("Hero") before package
+
     }
 }
--- src/main/kotlin/bignerdranch/interop/Hero.kt
+++ src/main/kotlin/bignerdranch/interop/Hero.kt
@@ -1,7 +1,7 @@
+@file:JvmName("Hero")
 package bignerdranch.interop
 
 import bignedranch.interop.Jhava
-
 
 fun main(args: Array<String>) {
     val adversary = Jhava()
@@ -12,4 +12,16 @@ fun main(args: Array<String>) {
 
     val adversaryHitPoints: Int = adversary.hitPoints
     println(adversaryHitPoints.dec())
+    println(adversaryHitPoints.javaClass)
+
+    adversary.greeting = "Hello, Hero."
+    println(adversary.utterGreeting())
+    println(adversary.greeting)
+}
+
+fun makeProclamation() = "Greetings, beast!"
+
+@JvmOverloads
+fun handOverFood(leftHand: String = "berries", rightHand: String = "beef") {
+    println("Mmmm... you hand over some delicious $leftHand and $rightHand.")
 }
