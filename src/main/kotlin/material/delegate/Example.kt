13d11ee4cc00be89ec528807634a0aef553028aa
3888 <krrg@mail.ru>
2021-01-23 15:16:02 +0200

    refactored Delegate example

--- src/main/kotlin/material/delegate/Example.kt
+++ src/main/kotlin/material/delegate/Example.kt
@@ -1,16 +1,23 @@
 package material.delegate
 
+import kotlin.random.Random
+
 
 fun main() {
-    val example = Example()
+//    print(trimDelegate("        123     "))
 
-    example.param = "        123     "
-
-    print(example.param)
-
-
+    println(lazyValue)
+    println(lazyValue)
 
 }
-class Example {
+
+val lazyValue: String by lazy {
+    println("Result of lambda = ${Random.nextInt(0, 10)}")
+    "Hello lazy String"
+}
+
+private fun trimDelegate(value: String): String {
     var param: String by TrimDelegate()
+    param = value
+    return param
 }
