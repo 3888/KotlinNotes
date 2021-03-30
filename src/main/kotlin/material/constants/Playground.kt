2fb21b5a9554cdb117a7000d850706e8ec363808
3888 <krrg@mail.ru>
2021-03-30 16:46:22 +0300

    [ADDED] EnumWithFunction

--- src/main/kotlin/material/constants/Playground.kt
+++ src/main/kotlin/material/constants/Playground.kt
@@ -1,11 +1,14 @@
 package material.constants
 
-import material.constants.enum.EnumColors
+import material.constants.enum.Coordinate
+import material.constants.enum.EnumWithFunction
 
 fun main() {
 
-    println(EnumColors.BLACK.colorHex)
-    println(EnumColors.BLACK.ordinal)
+//    println(EnumColors.BLACK.colorHex)
+//    println(EnumColors.BLACK.ordinal)
+
+    println(EnumWithFunction.EAST.updateCoordinate(Coordinate(1, 1)))
 
 
 }
