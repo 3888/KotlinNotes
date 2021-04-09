739bf36c6887ba66a5fd806ce611b8d210e1f342
3888 <krrg@mail.ru>
2021-04-09 15:13:10 +0300

    refactored rouning mode

--- src/main/kotlin/material/math/Rouinding.kt
+++ src/main/kotlin/material/math/Rouinding.kt
@@ -9,11 +9,18 @@ import java.text.DecimalFormat
 https://developer.android.com/reference/kotlin/java/math/RoundingMode
  */
 fun main() {
-example()
+//    decimalFormat()
+    decimalFormat2()
+
 }
 
-private fun example() {
+private fun decimalFormat() {
     val decimalFormat = DecimalFormat("#.##").apply { roundingMode = RoundingMode.HALF_UP }
     println(decimalFormat.format(BigDecimal("-1000.045")))
 
+}
+
+private fun decimalFormat2() {
+    val decimal = BigDecimal(3.14159265359).setScale(0, RoundingMode.HALF_EVEN)
+    println(decimal) // 3
 }
