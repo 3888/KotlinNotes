a768164eed90c69406afdd2835a8ff869aec5e23
3888 <krrg@mail.ru>
2021-07-11 17:41:33 +0300

    [ADDED] generics examples

--- src/main/kotlin/bignerdranch/sandbox/Barrel.kt
+++ src/main/kotlin/bignerdranch/sandbox/Barrel.kt
@@ -1,18 +1,25 @@
 package bignerdranch.sandbox
 
+class Barrel<T>(var item: T)
 //class Barrel<T>(val item: T)
 //class Barrel<out T>(val item: T)
-class Barrel<in T>( item: T)
+//class Barrel<in T>( item: T)
 
 fun main() {
     var fedoraBarrel: Barrel<Fedora> =
         Barrel(Fedora("a generic-looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
 
-//    lootBarrel = fedoraBarrel
-    fedoraBarrel = lootBarrel
+//    lootBarrel = fedoraBarrel // ERROR Type mismatch.
+   /*
+   при этом Loot вполне можно присвоить экземпляр Fedora
+    */
+    var loot: Loot = Fedora("a generic-looking fedora", 15) // Нет ошибки
 
-//    lootBarrel.item = Coin(15)
-//    val myFedora: Fedora = fedoraBarrel.item
-//    val myFedora: Fedora = lootBarrel.item
+//    fedoraBarrel = lootBarrel // ERROR Type mismatch.
+
+
+    lootBarrel.item = Coin(15)
+    val myFedora: Fedora = fedoraBarrel.item
+//    val myFedora2: Fedora = lootBarrel.item
 }
