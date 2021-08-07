c8f7bff8b4800d60692ba2126299767f77b9c6da
3888 <krrg@mail.ru>
2021-08-07 13:23:45 +0300

    refactored code

---
+++ src/main/kotlin/codewars/DuplicatedWordsCount.kt
@@ -0,0 +1,26 @@
+package codewars
+
+import java.util.*
+
+object DuplicatedWordsCount {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // Given String containing duplicate words
+        var input = "Java is a programming language. Python is also a programming language."
+        // Converting given String to lowerCase
+        input = input.toLowerCase()
+        /* Split the Input String into words using
+      built-in split() method */
+        val strArray = input.split(" ").toTypedArray()
+        /* Declare List of String that will
+      contain repeated words*/
+        val repeatedWords: MutableList<String> = ArrayList()
+        /* Declare HashSet of String that will
+      contain unique words */
+        val uniqueWords = HashSet<String>()
+        for (str in strArray) {
+            if (!uniqueWords.add(str)) repeatedWords.add(str)
+        }
+        println(repeatedWords.size)
+    }
+}
