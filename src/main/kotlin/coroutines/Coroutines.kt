0f3cdc76ed1983caa56dd10c7a57ca68e57afb3f
krrg <krrg3888@gmail.com>
2022-06-05 20:04:41 +0300

    refactored code

--- src/main/kotlin/coroutines/Coroutines.kt
+++ src/main/kotlin/coroutines/Coroutines.kt
@@ -5,29 +5,9 @@ import java.util.*
 
 fun main(): Unit = runBlocking {
 
-    /*
-    sequential examples
-    * */
-//    doWork(this)
-
-//    launch {
-//        doWork(this)
-//    }
-
-//    async {
-//        doWork(this)
-//    }
-
-    /*
-      parallel examples
-      * */
-
-    val deferredList: List<Deferred<String>> = List(10) {
-        async {
-            getNameWithDelay(it.toString())
-        }
-    }
-    deferredList.forEach { println(it.await()) }
+//sequentialExamples(this)
+//    parallelExamples(this)
+//    joinExample(this)
 }
 
 private suspend fun getNameWithDelay(name: String): String {
@@ -36,10 +16,43 @@ private suspend fun getNameWithDelay(name: String): String {
     return "delay $millis doWork $name"
 }
 
-
 private suspend fun doWork(scope: CoroutineScope) {
     println("Scope $scope")
-    repeat(10) {
+    println("Thread ${Thread.currentThread().name}")
+    repeat(5) {
         println(getNameWithDelay(it.toString()))
     }
+}
+
+private suspend fun sequentialExamples(scope: CoroutineScope) {
+//    doWork(scope)
+
+//    scope.launch {
+//        doWork(scope)
+//    }
+
+//    scope.async {
+//        doWork(scope)
+//    }
+}
+
+private suspend fun parallelExamples(scope: CoroutineScope) {
+    val deferredList: List<Deferred<String>> = List(10) {
+        scope.async {
+            getNameWithDelay(it.toString())
+        }
+    }
+    deferredList.forEach { println(it.await()) }
+}
+
+private suspend fun joinExample(scope: CoroutineScope) {
+    val job = scope.launch {
+        repeat(3) {
+            println("Coroutine count $it, it's still working  ${Thread.currentThread().name}")
+            delay(1000L)
+        }
+    }
+    println("job is join and waiting ")
+    job.join()
+    println("job is done  ${Thread.currentThread().name}")
 }
