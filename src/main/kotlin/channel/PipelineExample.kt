a859dacebd203f96160877167f7de9e4391a085b
3888 <krrg3888@gmail.com>
2022-12-11 09:27:42 +0200

    added Pipeline, Demultiplexer

--- src/main/kotlin/channel/PipelineExample.kt
+++ src/main/kotlin/channel/PipelineExample.kt
@@ -4,92 +4,75 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
+data class Fruit(override val name: String, override val color:
+String) : Item
+data class Vegetable(override val name: String, override val
+color: String) : Item
 
 @ExperimentalCoroutinesApi
 fun main() {
-    runBlocking {
-        val isItemsChannelLogged = false
-        val itemsChannel = produceItems(isItemsChannelLogged)
-        iterateItems(itemsChannel, "itemsChannel", isItemsChannelLogged)
-        println()
+    // ------------ Helper Methods ------------
+    fun isFruit(item: Item): Boolean = item is Fruit
+    fun isRed(item: Item): Boolean = (item.color == "Red")
+    // ------------ Pipeline ------------
+// 1
+    fun produceItems() = GlobalScope.produce {
+        val itemsArray = ArrayList<Item>()
+        itemsArray.add(Fruit("Apple", "Red"))
+        itemsArray.add(Vegetable("Zucchini", "Green"))
+        itemsArray.add(Fruit("Grapes", "Green"))
+        itemsArray.add(Vegetable("Radishes", "Red"))
+        itemsArray.add(Fruit("Banana", "Yellow"))
+        itemsArray.add(Fruit("Cherries", "Red"))
+        itemsArray.add(Vegetable("Broccoli ", "Green"))
 
-        val isFruitsChannelLogged = false
-        val fruitsChannel = produceFruits(produceItems(isFruitsChannelLogged))
-        iterateItems(fruitsChannel, "fruitsChannel", isFruitsChannelLogged)
-        println()
-
-        val isRedFruitsChannelLogged = true
-        val redFruitsChannel = produceRedFruits(produceFruits(produceItems(isRedFruitsChannelLogged)))
-        iterateItems(redFruitsChannel, "redFruitsChannel", isRedFruitsChannelLogged)
-
-        itemsChannel.cancel()
-        redFruitsChannel.cancel()
-        fruitsChannel.cancel()
-    }
-}
-
-private fun produceItems(isLogActive: Boolean) = GlobalScope.produce {
-    val itemsArray = ArrayList<Item2>()
-    itemsArray.add(Fruit2("Apple", true))
-    itemsArray.add(Fruit2("Strawberry", true))
-    itemsArray.add(Fruit2("Cherries", true))
-    itemsArray.add(Fruit2("Banana", false))
-    itemsArray.add(Fruit2("Grapes", false))
-
-    itemsArray.add(Vegetable2("Broccoli ", false))
-    itemsArray.add(Vegetable2("Zucchini", false))
-    itemsArray.add(Vegetable2("Radishes", true))
-
-    itemsArray.forEach {
-        delay(10)
-        if (isLogActive) {
-            val type = if (it is Vegetable2) "Vegetable" else "Fruit"
-            println("send ${it.name} type $type isRed ${it.isRed}")
+        itemsArray.add(Fruit("Strawberry", "Red"))
+// Send each item in the channel
+        itemsArray.forEach {
+            send(it)
         }
-        send(it)
     }
-}
-
-private fun produceFruits(items: ReceiveChannel<Item2>) = GlobalScope.produce {
-    for (item in items) {
+    // 2
+    fun isFruit(items: ReceiveChannel<Item>) = GlobalScope.produce {
+        for (item in items) {
 // Send each item in the channel only if it is a fruit
-        if (isFruit(item)) send(item)
-    }
-}
-
-private fun produceRedFruits(items: ReceiveChannel<Item2>) = GlobalScope.produce {
-    for (item in items) {
-// Send each item in the channel only if it is red in color
-        if (item.isRed) send(item)
-    }
-}
-
-private suspend fun iterateItems(channel: ReceiveChannel<Item2>, message: String, isLogActive: Boolean) {
-    if (isLogActive) {
-        for (item in channel) {
-            delay(10)
-            println("$message ReceiveChannel item ${item.name}  isRed ${item.isRed}")
+            if (isFruit(item)) {
+                send(item)
+            }
         }
     }
+    // 3
+    fun isRed(items: ReceiveChannel<Item>) = GlobalScope.produce {
+        for (item in items) {
+// Send each item in the channel only if it is red in color
+            if (isRed(item)) {
+                send(item)
+            }
+        }
+    }
+    runBlocking {
+// 4
+        val itemsChannel = produceItems()
+// 5
+        val fruitsChannel = isFruit(itemsChannel)
+// 6
+        val redChannel = isRed(fruitsChannel)
+// 7
+        for (item in redChannel) {
+            print("${item.name}, ")
+        }
+// 8
+        redChannel.cancel()
+        fruitsChannel.cancel()
+        itemsChannel.cancel()
+// 9
+        println("Done!")
+    }
 }
 
-private fun isFruit(item: Item2): Boolean = item is Fruit2
-
-private data class Fruit2(
-    override val name: String, override val isRed:
-    Boolean
-) : Item2
-
-private data class Vegetable2(
-    override val name: String, override val
-    isRed: Boolean
-) : Item2
-
-private interface Item2 {
+interface Item {
     val name: String
-    val isRed: Boolean
+    val color: String
 }
-
