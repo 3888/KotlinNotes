6458946d39dc68e4667ecb932c80e7d284a4a4d6
3888 <krrg3888@gmail.com>
2023-10-09 17:06:42 +0300

    refactored code

--- src/main/kotlin/contest/codewars/level7kyu/MaxLenDiff.kt
+++ src/main/kotlin/contest/codewars/level7kyu/MaxLenDiff.kt
@@ -1,16 +1,29 @@
 package contest.codewars.level7kyu
 
 import java.util.Collections
+import kotlin.math.abs
+import kotlin.math.max
 
 fun main() {
-    val a1 = arrayOf("hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz")
+    val a1 = arrayOf(
+        "hoqq",
+        "bbllkw",
+        "oox",
+        "ejjuyyy",
+        "plmiis",
+        "xxxzgpsssa",
+        "xxwwkktt",
+        "znnnnfqknaz",
+        "qqquuhii",
+        "dvvvwz"
+    )
     val a2 = arrayOf("cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww") // 13
 
     val a11 = arrayOf("ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh")
     val a22 = arrayOf("bbbaaayddqbbrrrv") // 10
 
-    mxdiflgMy(a1, a2)
-    mxdiflgMy(a11, a22)
+//    mxdiflg2(a1, a2)
+    mxdiflg2(a11, a22)
 
 }
 
@@ -30,6 +43,23 @@ private fun mxdiflgMy(a1: Array<String>, a2: Array<String>): Int {
     }
 }
 
-fun mxdiflg(a1:Array<String>, a2:Array<String>)
-        = a1.flatMap { s1 -> a2.map { kotlin.math.abs(s1.length - it.length) } }.maxOrNull() ?: -1
+fun mxdiflg(a1: Array<String>, a2: Array<String>) =
+    a1.flatMap { s1 -> a2.map { kotlin.math.abs(s1.length - it.length) } }.maxOrNull() ?: -1
 
+fun mxdiflg2(a1: Array<String>, a2: Array<String>): Int {
+    var m = -1
+    for (x in a1) {
+        println("x $x")
+        println("x ${x.length}")
+        for (y in a2) {
+            println("y $y")
+            println("y ${y.length}")
+            m = max(m, abs(x.length - y.length))
+            println("x.length - y.length ${x.length - y.length}")
+            println("abs x.length - y.length ${abs(x.length - y.length)}")
+            println("max m = $m & x.length - y.length = ${abs(x.length - y.length)} is $m")
+        }
+    }
+
+    return m
+}
