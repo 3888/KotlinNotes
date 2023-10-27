34b0385590665639d482229130c0c37c1b59ab78
3888 <krrg3888@gmail.com>
2023-10-27 16:43:35 +0300

    added 7 kyu

--- src/main/kotlin/contest/codewars/level7kyu/7kyu.kt
+++ src/main/kotlin/contest/codewars/level7kyu/7kyu.kt
@@ -1,14 +1,37 @@
 package contest.codewars.level7kyu
 
-import kotlin.math.abs
 import kotlin.math.sqrt
 
 
 fun main() {
+    println(
 
+    )
 
 }
 
+fun leaderBoard(user: String, userScore: Int, yourScore: Int): String {
+    val aim = userScore - yourScore
+    return when {
+        aim < 0 -> "Winning!"
+        aim == 0 -> "Only need one!"
+        aim / 3 + 1 > 1000 -> "To beat $user's score, I must complete ${aim / 3} Beta kata and ${aim % 3} 8kyu kata. Dammit!"
+        else -> "To beat $user's score, I must complete ${aim / 3} Beta kata and ${aim % 3} 8kyu kata."
+    }
+}
+
+private fun findScreenHeightMy(width: Int, ratio: String): String =
+    "$width" + "x" + "${
+        ((width.toDouble() / ratio.split(":").first().toInt()) * ratio.last().toString().toInt()).toInt()
+    }"
+
+fun findScreenHeight(width: Int, ratio: String): String {
+    val (w, h) = ratio.split(":").map { it.toInt() }
+    return "${width}x${width * h / w}"
+}
+
+fun findScreenHeight2(width: Int, ratio: String) =
+    "${width}x${ratio.split(":").let { width * it[1].toInt() / it[0].toInt() }}"
 
 private fun smallEnoughMy(a: IntArray, limit: Int): Boolean = a.sorted().last() <= limit
 private fun smallEnoughMy2(a: IntArray, limit: Int): Boolean = a.maxOrNull()!! <= limit
@@ -33,7 +56,8 @@ fun fizzBuzzMy(n: Int): Array<String> = Array(n) {
 }
 
 fun fizzBuzz(n: Int) = (1..n).map {
-    when{it % 15 == 0 -> "FizzBuzz"
+    when {
+        it % 15 == 0 -> "FizzBuzz"
         it % 5 == 0 -> "Buzz"
         it % 3 == 0 -> "Fizz"
         else -> "$it"
